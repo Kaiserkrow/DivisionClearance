68f851f2c18c6b3ae63e3a3e336b9a7356c54e33
Andrew Garcia <42108060+Kaiserkrow@users.noreply.github.com>
2025-07-24 09:09:09 +0800

    sql added

---
+++ sql/divisionclearance.sql
@@ -0,0 +1,61 @@
+-- MySQL dump 10.13  Distrib 8.0.42, for Win64 (x86_64)
+--
+-- Host: localhost    Database: divisionclearance
+-- ------------------------------------------------------
+-- Server version	8.0.42
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `entries`
+--
+
+DROP TABLE IF EXISTS `entries`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `entries` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `fullName` varchar(100) COLLATE utf8mb4_general_ci NOT NULL,
+  `position` varchar(100) COLLATE utf8mb4_general_ci NOT NULL,
+  `school` varchar(100) COLLATE utf8mb4_general_ci NOT NULL,
+  `district` varchar(100) COLLATE utf8mb4_general_ci NOT NULL,
+  `purposeOfClearance` varchar(100) COLLATE utf8mb4_general_ci NOT NULL,
+  `additionalNote` text COLLATE utf8mb4_general_ci NOT NULL,
+  `dateOfAction` date DEFAULT NULL,
+  `startDate` date DEFAULT NULL,
+  `endDate` date DEFAULT NULL,
+  `divisionSigned` date NOT NULL,
+  `schoolDistrictSigned` date NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=72 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `entries`
+--
+
+LOCK TABLES `entries` WRITE;
+/*!40000 ALTER TABLE `entries` DISABLE KEYS */;
+INSERT INTO `entries` VALUES (64,'example 2','example 2','Alawihao ES','DAET NORTH','retirement','N/A','2025-07-24',NULL,NULL,'2025-07-20','2025-07-19'),(65,'Example example 3','example 3','Maximo Manarang HS','PARACALE','resigned','N/A','2025-07-30',NULL,NULL,'2025-07-29','2025-07-24'),(66,'Example 4','example 4','Labo National High School','LABO - A','resigned','N/A','2025-07-31',NULL,NULL,'2025-07-23','2025-07-22'),(67,'example 5','Teacher I','Gumaus ES','PARACALE','sick Leave','N/A',NULL,'2025-07-21','2025-07-23','2025-07-21','2025-07-20'),(68,'example 6','Teacher II','Salvacion Big ES','SAN VICENTE - SAN LORENZO RUIZ','transferred Out','Papunta na sa example','2025-07-24',NULL,NULL,'2025-07-23','2025-07-22'),(69,'example 7 ','Teacher III','Macolabo ES','PARACALE','travel','Example 7',NULL,'2025-07-24','2025-07-28','2025-07-24','2025-07-23'),(70,'Example 8','example 8 ','Mancruz ES','DAET SOUTH','retirement','N/A','2025-08-09',NULL,NULL,'2025-07-26','2025-07-25'),(71,'Example 9 ','example 9','Potenciano Juego ES','CAPALONGA','sick Leave','N/A',NULL,'2025-07-24','2025-07-28','2025-07-22','2025-07-19');
+/*!40000 ALTER TABLE `entries` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2025-07-24  9:05:09
